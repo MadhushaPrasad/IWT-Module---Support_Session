a5ba52730337b9875ec436a0c137b7c02d285eb5
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-11-21 16:45:49 +0530

    update past paper Q2->Qa

--- 22nd_November/pastpaper/2022/Question02/Qa/index.php
+++ 22nd_November/pastpaper/2022/Question02/Qa/index.php
@@ -1,17 +1,17 @@
 <?php
 
 //store students mark in associate array
-$studentDetails = array("Uthpala" => "80", "Meena" => "92", "Abdhul" => "85", "Sahan" => "50", "Teena" => "65");
+$studentDetails = array("Uthpala" => "80", "Meena" => "92", "Abdhul" => "85", "Sahan" => "50", "Teena" => "65");//associative array
 
-foreach ($studentDetails as $name => $mark) {
+foreach ($studentDetails as $name => $mark) {//loop through the array
 
-    if ($mark >= 90) {
-        echo "Congratulation " . $name . " You got " . $mark . " marks and your grade is A";
-    } elseif ($mark >= 80) {
+    if ($mark >= 90) {// if mark is greater than 90
+        echo "Congratulation " . $name . " You got " . $mark . " marks and your grade is A";//print the message
+    } elseif ($mark >= 80) {// if mark is greater than 80
         echo "Congratulation " . $name . " You got " . $mark . " marks and your grade is B";
-    } elseif ($mark >= 70) {
+    } elseif ($mark >= 70) {// if mark is greater than 70
         echo "Congratulation " . $name . " You got " . $mark . " marks and your grade is C";
-    } elseif ($mark >= 60) {
+    } elseif ($mark >= 60) {// if mark is greater than 60
         echo "Congratulation " . $name . " You got " . $mark . " marks and your grade is D";
     } else {
         echo "Try harder " . $name . ", You got " . $mark . " marks and you Fail the module";
